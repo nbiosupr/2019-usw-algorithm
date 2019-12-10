b727efbd8f05f86b2efc5e2e777a7c868da27602
Sangwoon Park <nbiosupr@gmail.com>
2019-12-10 15:59:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import quick_sort
 import merge_sort
+import counting_sort
+import selection_sort
 import sort_tester
 
 print("-"*10)
 print("<quick sort>\n")
 sort_tester.test(quick_sort.sort)
-
 print("-"*10)
 print("<merge sort>\n")
 sort_tester.test(merge_sort.sort)
+print("-"*10)
+print("<count sort>\n")
+sort_tester.test(counting_sort.sort)
+print("-"*10)
+print("<selection sort>\n")
+sort_tester.test(selection_sort.sort)
